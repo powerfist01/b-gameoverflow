8df952cd2be0f0a7b8544acc7e8b4b051867d374
powerfist01 <sujeets207@gmail.com>
2021-03-10 21:24:08 +0530

    Old update

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(logger('dev'));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var indexRouter = require('./api/routes/index');
+app.use(logger('dev'));
+// app.use(cookieParser());
+
+var indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
--- config/config.js
+++
@@ -1,5 +0,0 @@
-mongoUri = 'mongodb://localhost:27017/gameOverflow'
-
-module.exports = {
-  mongoUri: mongoUri
-};
