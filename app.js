444e80ccdfa82596104a935924b6fbe67e8ee60a
powerfist01 <sujeets207@gmail.com>
2021-04-24 00:32:40 +0530

    :bug: Added comments

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(logger('dev'));
+app.use(logger('dev'));    // configire morgan
 
 const indexRouter = require('./routes/index');
 const questionRouter = require('./routes/questions')(express);
